f55be5dbaf427bfcbade752629bd182808716e07
219 <lhg2196@naver.com>
2025-09-02 08:50:50 +0900

    mvc 추가

--- servlet/src/main/java/hello2/servlet/web/springmvc/old/OldController.java
+++ servlet/src/main/java/hello2/servlet/web/springmvc/old/OldController.java
@@ -1,14 +1,16 @@
-package hello2.servlet.web.Springmvc;
-
+package hello2.servlet.web.springmvc.old;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+@Component("/springmvc/old-controller")
 public class OldController implements Controller {
     @Override
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public ModelAndView handleRequest(HttpServletRequest request,
+                                      HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form");
     }
 }
